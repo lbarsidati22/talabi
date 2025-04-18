8293255c129025397da44959d35d468c234a3831
lbarsidati22 <lbarsidati575@gmail.com>
2025-04-18 18:31:38 +0000

    add supabase done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:talabi/config.dart';
 import 'package:talabi/core/utils/theme/app_theme.dart';
 import 'package:talabi/views/auth/ui/login_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: Config.supabaseUrl,
+    anonKey: Config.supabaseAnonKey,
+  );
   runApp(const Talabi());
 }
 
