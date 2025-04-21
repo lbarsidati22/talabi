19384f4ef5e6a115b7f6dde0a355ec0407b9cebc
lbarsidati22 <lbarsidati575@gmail.com>
2025-04-21 00:16:17 +0000

    rate done

--- lib/views/prudact_details/logic/cubit/product_deatils_cubit.dart
+++ lib/views/prudact_details/logic/cubit/product_deatils_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:talabi/core/services/api_services.dart';
 import 'package:talabi/views/prudact_details/logic/models/rate_model.dart';
@@ -9,10 +10,18 @@ class ProductDeatilsCubit extends Cubit<ProductDeatilsState> {
   ProductDeatilsCubit() : super(ProductDeatilsInitial());
   final ApiServices _apiServices = ApiServices();
   List<RateModel> rates = [];
-  Future<void> getRate() async {
+  Future<void> getRate({required String productId}) async {
     emit(GetRateLeading());
     try {
-      Response response = await _apiServices.getData('');
-    } catch (e) {}
+      Response response = await _apiServices
+          .getData('rates_table?select=*&for_product=eq.$productId');
+      for (var rate in response.data) {
+        rates.add(RateModel.fromJson(rate));
+      }
+      emit(GetRateSuccess());
+    } catch (e) {
+      emit(GetRateError());
+      debugPrint(e.toString());
+    }
   }
 }
